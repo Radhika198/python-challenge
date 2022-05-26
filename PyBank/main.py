4fce0ccb771d6bd1bdba83106e86daae0d9d6d7f
Radhika198 <gradhikababu@gmail.com>
2022-05-26 11:47:30 +1000

    created text file

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,21 +1,21 @@
-#import budget data 
 import os
 import csv 
 
 #Path to collect data from the Resources folder
-csvpath=os.path.join("Resources","budget_data.csv")
+csvpath=os.path.join('Resources','budget_data.csv')
 
 # Open and read csv
 with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
      # Read the header row first
     csv_header = next(csvreader)
+
     rows_list = list(csvreader)
     
     #Declaring variables
-    month = []
-    revenue = []
-    rev_change = []
+    months = []
+    profit_and_loss = []
+    profit_loss_change = []
     Avg_change = []
                    
     # Read through each row of data after the header and count number of months    
@@ -23,40 +23,49 @@ with open(csvpath, newline='') as csvfile:
          # Skipping empty rows which also helps data cleansing process
         if(len(row) < 1):
                continue
-        # Appending months(column 1) into month variable
-        month.append(row[0])
-        # Appending revenue(column 2) into revenue variable
-        revenue.append(row[1])
+        # Appending months into months variable
+        months.append(row[0])
+        # Appending profit and loss  into profit and loss variable
+        profit_and_loss.append(row[1])
 
-    #Sum of Revenue 
-    rev_int = map(int,revenue)
-    tot_revenue = (sum(rev_int))
+    #Calculate Total value of profit and loss 
+    profit_loss_int = map(int,profit_and_loss)
+    total_profit_and_loss = (sum(profit_loss_int))
   
-    # Average Change
+    # Calculate Average Change value of profit and loss
     i = 0
-    for i in range(len(revenue) - 1):
-        profit_loss = int(revenue[i+1]) - int(revenue[i])
+    for i in range(len(profit_and_loss) - 1):
+        profit_loss_diff = int(profit_and_loss[i+1]) - int(profit_and_loss[i])
         # append profit_loss
-        rev_change.append(profit_loss)
-    Total = sum(rev_change)
-    # Calculate Average change
-    Avg_change = Total / len(rev_change)
+        profit_loss_change.append(profit_loss_diff)
+    Total = sum(profit_loss_change)
+    Avg_change = Total/len(profit_loss_change)
 
-#Greatest Increase
-    profit_increase = max(rev_change)
-    k = rev_change.index(profit_increase)
-    mon_increase = month[k+1]
+    #Determine Greatest Increase in Profits
+    greatest_profit_increase = max(profit_loss_change)
+    k = profit_loss_change.index(greatest_profit_increase)
+    month_of_increase = months[k+1]
     
-#Greatest Decrease
-    profit_decrease = min(rev_change)
-    j = rev_change.index(profit_decrease)
-    mon_decrease = month[j+1]
+    #Determine Greatest Decrease in Losses
+    greatest_loss_decrease = min(profit_loss_change)
+    j = profit_loss_change.index(greatest_loss_decrease)
+    month_of_decrease = months[j+1]
 
-#Print Outcome
+#Print Outcome of Financial Analysis
 print(f'Financial Analysis'+'\n')
 print(f'----------------------------'+'\n')
-print("Total months: " + str(len(month)))
-print("Total: $" + str(tot_revenue)) 
+print("Total months: " + str(len(months)))
+print("Total: $" + str(total_profit_and_loss)) 
 print(f'Average Change: ${Avg_change:.2f}')
-print(f"Greatest Increase in Profits: {mon_increase} (${profit_increase})")
-print(f"Greatest Decrease in Profits: {mon_decrease} (${profit_decrease})")
+print(f"Greatest Increase in Profits: {month_of_increase} (${greatest_profit_increase})")
+print(f"Greatest Decrease in Profits: {month_of_decrease} (${greatest_loss_decrease})")
+
+#Exporting the Financial analysis result into text file
+with open('Analysis', 'financial_analysis.txt', 'w') as text:
+    text.write(f'Financial Analysis'+'\n')
+    text.write(f'----------------------------'+'\n')
+    #text.write("Total months: " + str(len(months)))
+    #text.write("Total: $" + str(total_profit_and_loss)) 
+    text.write(f'Average Change: ${Avg_change:.2f}')
+    text.write(f"Greatest Increase in Profits: {month_of_increase} (${greatest_profit_increase})")
+    text.write(f"Greatest Decrease in Profits: {month_of_decrease} (${greatest_loss_decrease})")
